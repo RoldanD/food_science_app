4078fce8b7d7d058e59ffca9cfa609dbc0dad5bb
Roldan Descamps <descamps.roldan@gmail.com>
2016-01-13 13:49:52 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FoodScienceApp::Application.config.secret_token = 'd2981cc084aa4edd5cbde49aafc92081a18a55362093fca43e7f4b620edc05c97657bba6dc6b14e5c8f169011e3cdef6c8c8d03a543c90367584ab6dbc20b9d2'
